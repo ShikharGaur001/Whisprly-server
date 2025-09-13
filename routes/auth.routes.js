ec2458d009eeb7e547c6aae1194f10849db54adf
Shikhar Gaur <shikhargaur2008@gmail.com>
2025-09-13 23:04:43 +0530

    moving to profile ui in client side

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { signin, signup } from "../controllers/auth.controller.js";
+import { getUserInfo, signin, signup } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
-const authRoutes = Router()
+const authRoutes = Router();
 
-authRoutes.post("/signup", signup)
-authRoutes.post("/signin", signin)
+authRoutes.post("/signup", signup);
+authRoutes.post("/signin", signin);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
 
-export default authRoutes
+export default authRoutes;
